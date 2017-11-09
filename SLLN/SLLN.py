266b76e107218bfb1fa900d8392696dac871ecca
Michael Valentin <michael.valentin1136@gmail.com>
2017-11-09 09:15:20 -0400

    Code Done

--- SLLN/SLLN.py
+++ SLLN/SLLN.py
@@ -1,1 +1,24 @@
+# Single Linked List #
+
+class SingleLinkedListNode(object):
+
+    #Inizializarmos
+    def _init_(self, value, nxt):
+        self.value = value
+        self.next = nxt
+
+    def _repr_(self):
+        nval = self.next and self.next.value or None
+        return f"[{self.value}:{repr(nval)}]"
+        
+        
+SLLN=SingleLinkedListNode()
+SLLN1=SingleLinkedListNode()
+
+SLLN.value="a"
+SLLN1.value="b"
+SLLN.nxt=SLLN1
+SLLN1.nxt=None
+print (SLLN.value)
+print(SLLN.nxt.value)
 
